199975ca6abf134fc776d4017196a047091ba02f
Aleksander Nygård Tonheim <>
2018-06-23 18:28:50 +0200

    Update in NS CLI

---
+++ appMilka/platforms/ios/build/appMilka.build/Debug-iphonesimulator/appMilka.build/Objects-normal/i386/main.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Users/aleksandernygardtonheim/assessmentMilka/appMilka/platforms/ios/appMilka/appMilka-Prefix.pch \
+  /Users/aleksandernygardtonheim/assessmentMilka/appMilka/platforms/ios/internal/main.m \
+  /Users/aleksandernygardtonheim/assessmentMilka/appMilka/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/aleksandernygardtonheim/assessmentMilka/appMilka/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/aleksandernygardtonheim/assessmentMilka/appMilka/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/aleksandernygardtonheim/assessmentMilka/appMilka/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/aleksandernygardtonheim/assessmentMilka/appMilka/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntimeInstrumentation.h \
+  /Users/aleksandernygardtonheim/assessmentMilka/appMilka/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/aleksandernygardtonheim/assessmentMilka/appMilka/platforms/ios/internal/TNSDebugging.h \
+  /Users/aleksandernygardtonheim/assessmentMilka/appMilka/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
