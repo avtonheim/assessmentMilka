05e066a0c0208c6fb165b4f92730495d06a3c3a3
Aleksander Nygård Tonheim <>
2018-06-24 14:01:59 +0200

    Added first questions, styling, database and the chart plug-in to represent data.

--- appMilka/app/main-page.js
+++ appMilka/app/main-page.js
@@ -1,8 +1,21 @@
-
-var createViewModel = require("./main-view-model").createViewModel;
+var frameModule = require('ui/frame');
 
 function onNavigatingTo(args) {
     var page = args.object;
-    page.bindingContext = createViewModel();
+    //Controlling the native back-button
+  var controller = frameModule.topmost().ios.controller;
+  var navigationItem = controller.visibleViewController.navigationItem;
+  navigationItem.setHidesBackButtonAnimated(true, false);
 }
 exports.onNavigatingTo = onNavigatingTo;
+
+
+function navigateQuestions(){
+  frameModule.topmost().navigate('views/questions/questions');
+}
+exports.navigateQuestions = navigateQuestions;
+
+function navigateSummary(){
+  frameModule.topmost().navigate('views/summary/summary');
+}
+exports.navigateSummary = navigateSummary;
